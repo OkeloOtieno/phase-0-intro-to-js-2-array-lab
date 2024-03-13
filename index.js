2052c5cc9b94ce34e3e621069ac6be49bfd5bdd8
Emmanuel <emmanuelokello294@gmail.com>
2024-03-13 15:15:00 +0300

    completes intro 2 arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+let cats = [
+    "Milo","Otis","Garfield"
+]
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+function appendCat(name) {
+    const otherCats = [...cats, "Broom"]
+    return otherCats
+}
+function prependCat(name) {
+    const anotherGroup = ["Arnold", ...cats]
+    return anotherGroup
+}
+function removeLastCat() {
+    const lastCatsss = cats.slice(0, -1) 
+    return lastCatsss
+
+}
+function removeFirstCat() {
+    const firstCatss = cats.slice(1)
+    return firstCatss
+}
